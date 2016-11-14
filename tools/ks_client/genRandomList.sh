f29878ebdce13f3035a66c1b4b7ac586557b0e57
Javier Gusano Martínez <jgm1986@users.noreply.github.com>
2016-11-14 18:08:54 +0100

    This script creates a text flie with random fingerprints from Redis database.

---
+++ tools/ks_client/genRandomList.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+COUNTER=0
+NUMCERT=1
+
+if [ $# -eq 1 ] ; then
+        NUMCERT=$1
+fi
+
+while [ $COUNTER -lt $NUMCERT ]; do
+        src/redis-cli -a foobared -n 0 RANDOMKEY >> pklist.txt
+        let COUNTER=COUNTER+1
+done
+
+echo "[ OK ]"
+
+exit 0
