f8a709405b3165dea9b9b5d122d2a75a3c5283b5
Javier Gusano Martinez <jgm1986@hotmail.com>
2016-11-30 18:30:56 +0100

    Removed unnecessary installation dependencies from update script.

--- tools/updates/update.sh
+++ tools/updates/update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install curl build-essential libcurl4-openssl-dev openssl autoconf libtool -y
+#sudo apt-get install curl build-essential libcurl4-openssl-dev openssl autoconf libtool -y
+sudo apt-get install curl -y
 KSPKG=$(curl -L https://github.com/mami-project/KeyServer/releases/latest | grep .jar\"  | cut -d "\"" -f2 | cut -d "\"" -f1)
 DOWNLOAD=https://github.com$KSPKG
 echo "DEBUG: $DOWNLOAD" 
